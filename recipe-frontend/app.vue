3b32313c5dcfe1bd2c99a8cefb7c62c3052051e1
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-04-28 17:34:04 +0300

    setup with tailwind

--- recipe-frontend/app.vue
+++ recipe-frontend/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-black text-white h-screen">
+Hello Again Nati
   </div>
 </template>
--- recipe-frontend/nuxt.config.ts
+++ recipe-frontend/nuxt.config.ts
@@ -2,13 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
+  modules: ['@nuxtjs/tailwindcss']
 })
